3737af697d5e6388db3863bb5a26806c5100dbe6
Moe Farid <moe.faridd@gmail.com>
2021-03-09 23:51:56 -0500

    commit

--- app.js
+++ app.js
@@ -45,3 +45,13 @@ const addNum = (n) => {
 }
 
 console.log(addNum(6))
+
+const addNum = (n) => {
+  let total = 0
+  for (let i = 0; i <= n; i++) {
+    total += i
+  }
+  return total
+}
+
+console.log(addNum(6))
